9badc197ae5cd61034e69aa5bd978dd6d8113d9e
KOSEKI Kengo <koseki@gmail.com>
2008-01-15 11:44:52 +0000

    manifestコマンドを実装しました。

--- lib/smallcage/commands/manifest.rb
+++ lib/smallcage/commands/manifest.rb
@@ -9,7 +9,31 @@ module SmallCage::Commands
     end
     
     def execute
+      entries = []
+      root = Pathname.new(@opts[:path])
+      Dir.chdir(root) do
+        Dir.glob("**/*") do |f|
+          entries << f
+        end
+      end
       
+      tmp = []
+      entries.each do |f|
+        path = root + f
+        f = "./" + f
+        if path.directory?
+          f = f + "/"
+        end
+        next if path.basename.to_s == "Manifest.html"
+        tmp << f
+      end
+      entries = tmp
+      
+      template = File.dirname(__FILE__) + "/Manifest.erb"
+      source = ERB.new(File.read(template), nil, "-").result(binding)
+      open(root + "Manifest.html", "w") do |io|
+        io << source
+      end
     end
   end
 end
