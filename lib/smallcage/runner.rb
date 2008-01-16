4e495cfd65f6c063d372e9c784faa698e3f46fb0
KOSEKI Kengo <koseki@gmail.com>
2008-01-16 07:58:07 +0000

    コマンドクラスのrequireを最初に実行するようにしました。chdirでrequireできなくなるためですが、遅くなってしまうので考え中です。

--- lib/smallcage/runner.rb
+++ lib/smallcage/runner.rb
@@ -9,32 +9,26 @@ module SmallCage
     end
     
     def update
-      require 'smallcage/commands/update'
       SmallCage::Commands::Update.execute(@opts)
     end
     
     def clean
-      require 'smallcage/commands/clean'
       SmallCage::Commands::Clean.execute(@opts)
     end
     
     def server
-      require 'smallcage/commands/server'
       SmallCage::Commands::Server.execute(@opts)
     end
 
     def auto
-      require 'smallcage/commands/auto'
       SmallCage::Commands::Auto.execute(@opts)
     end
     
     def import
-      require 'smallcage/commands/import'
       SmallCage::Commands::Import.execute(@opts)
     end
     
     def manifest
-      require 'smallcage/commands/manifest'
       SmallCage::Commands::Manifest.execute(@opts)
     end
 
