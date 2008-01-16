f64ef356319048b8f9941d134cbe706942befaf5
KOSEKI Kengo <koseki@gmail.com>
2008-01-16 07:57:12 +0000

    コマンドクラスのrequireを最初に実行するようにしました。chdirでrequireできなくなるためですが、遅くなってしまうので考え中です。

--- lib/smallcage.rb
+++ lib/smallcage.rb
@@ -3,11 +3,19 @@ $:.unshift File.dirname(__FILE__)
 require 'yaml'
 require 'erb'
 require 'pathname'
+require 'open-uri'
 
 require 'smallcage/version'
 require 'smallcage/misc'
 require 'smallcage/loader'
 require 'smallcage/erb_base'
 require 'smallcage/renderer'
-
 require 'smallcage/runner'
+
+require 'smallcage/commands/update'
+require 'smallcage/commands/clean'
+require 'smallcage/commands/server'
+require 'smallcage/commands/auto'
+require 'smallcage/commands/import'
+require 'smallcage/commands/manifest'
+
