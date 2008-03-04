61c2040357e43f96c3e5f66f1d66610c81439786
KOSEKI Kengo <koseki@gmail.com>
2008-03-04 17:04:53 +0000

    export directory must not exist.

--- lib/smallcage/commands/export.rb
+++ lib/smallcage/commands/export.rb
@@ -3,14 +3,25 @@ module SmallCage::Commands
     def execute
       target = Pathname.new(@opts[:path])
       unless target.exist?
-        raise "target directory or file does not exist.: " + target.to_s
+        raise target.to_s + " does not exist."
       end
-      
-      out = Pathname.new(@opts[:out]).realpath
       
       loader = SmallCage::Loader.new(target)
       root = loader.root
       
+      if @opts[:out].nil?
+        out = root + ("./_smc/tmp/" + Time.now.strftime("%Y%m%d%H%M%S"))
+      else
+        out = Pathname.new(@opts[:out])
+      end
+      if out.exist?
+        raise out.to_s + " already exist."
+      end
+      FileUtils.makedirs(out)
+      out = out.realpath
+      
+      
+      # TODO create empty directories
       loader.each_not_smc_file do |docpath|
         dir = Pathname.new(docpath.uri).parent
         outdir = out + ("." + dir.to_s)
