d2ce3cc5df7b205763e5c83ad5d05ddf4272dc33
KOSEKI Kengo <koseki@gmail.com>
2008-03-04 19:32:51 +0000

    delete output file which has no source file.

--- lib/smallcage/commands/update.rb
+++ lib/smallcage/commands/update.rb
@@ -17,9 +17,13 @@ module SmallCage::Commands
       loader = SmallCage::Loader.new(target)
       renderer = SmallCage::Renderer.new(loader)
       
+      urilist = []
+      
       loader.each_smc_obj do |obj|
+        urilist << obj["uri"].smc
+        
         result = renderer.render(obj["template"], obj)
-              
+        
         filters = loader.filters("after_rendering_filters")
         filters.each do |f|
           result = f.after_rendering_filter(obj, result)
@@ -27,6 +31,28 @@ module SmallCage::Commands
 
         output_result(obj, result)
         puts obj["uri"] if @opts[:quiet].nil?
+      end
+      
+      listfile = loader.root + "./_smc/tmp/list.txt"
+      if listfile.exist?
+        txt = File.read(listfile)
+        old_urilist = txt.split(/\n/)
+        old_urilist.shift
+        deletelist = old_urilist - urilist
+        deletelist.each do |uri|
+          delfile = SmallCage::DocumentPath.new(loader.root, loader.root + ("." + uri)).outfile
+          if delfile.path.file?
+            puts "delete: #{delfile.uri}"
+            File.delete(delfile.path)
+          end
+        end
+      end
+      FileUtils.makedirs(listfile.parent)
+      open(listfile, "w") do |io|
+        io << "version: " + SmallCage::VERSION::STRING + "\n"
+        urilist.each do |u|
+          io << u + "\n"
+        end
       end
     end
     
