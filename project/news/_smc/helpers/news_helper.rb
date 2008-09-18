4574bdcc50373ff5efa4b7ffe3cd57a8be4c2844
KOSEKI Kengo <koseki@gmail.com>
2008-09-18 13:31:34 +0000

    add news helper.

---
+++ project/news/_smc/helpers/news_helper.rb
@@ -0,0 +1,34 @@
+module SmallCage
+
+  # news entry file ... YYYYMMDDHHMM.html.smc
+  # archive file    ... YYYY.html.smc or YYYYMM.html.smc or YYYYMMDD.html.smc
+  module NewsHelper
+
+    def each_latest_news(amount, &block)
+      i = 0
+      _glob(".", %r{/\d{12}\.html\.smc$}).reverse.each do |path|
+        _with(_load(path), &block)
+        i += 1
+        break if amount <= i
+      end
+    end
+    
+    def each_archived_news(&block)
+      date = @obj["uri"].match(%r{/(\d{4})(\d{2})?(\d{2})?\.html$}).to_a
+      date = date.join("")
+      _glob(".", %r{/#{date}\d{#{12-date.length}}\.html\.smc$}).reverse.each do |path|
+        _with(_load(path), &block)
+      end
+    end
+
+    def news_time
+      date = @obj["uri"].match(%r{/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})\.html}).to_a.map {|d| d.to_i }
+      return Time.local(*date)
+    end
+    
+    def news_ftime(format = "%Y-%m-%d")
+      news_time().strftime(format)
+    end
+    
+  end
+end
