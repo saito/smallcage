1fabb823e379f1d06e65b657d963347ef92bb95b
koseki <koseki@gmail.com>
2009-05-05 01:23:24 +0900

    simpler

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,4 @@
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
 
-begin
-  require 'spec'
-rescue LoadError
-  require 'rubygems'
-  gem 'rspec'
-  require 'spec'
-end
+require 'rubygems'
+require 'spec'
