ab63535f3edd9d2f31b249c8657390596d1b51a0
koseki <koseki@gmail.com>
2009-08-13 04:00:51 +0900

    read charset from smc object or dir config

--- project/nkf/_smc/filters/nkf_filter.rb
+++ project/nkf/_smc/filters/nkf_filter.rb
@@ -1,14 +1,42 @@
+require "nkf"
+
 module SmallCage
-  require "nkf"
-  
+
+  # In _dir.smc or other *.smc, set charset like this:
+  # 
+  #   charset: Shift_JIS
+  #   
   class NkfFilter
 
     def initialize(opts)
-      @args = opts["args"]
     end
 
     def after_rendering_filter(obj, str)
-      return NKF.nkf(@args, str)
+      charset = ""
+      if obj["charset"]
+        charset = obj["charset"]
+      else
+        obj["dirs"].reverse.each do |dir|
+          if dir["charset"]
+            charset = dir["charset"]
+            break
+          end
+        end
+      end
+
+      opt = ""
+      if charset =~ /^euc-jp$/i
+        opt = "-Wem0"
+      elsif charset =~ /^iso-2022-jp$/i
+        opt = "-Wjm0"
+      elsif charset =~ /^shift_jis$/i
+        opt = "-Wsm0"
+      else
+        STDERR.puts "Unknown charset: #{charset}" unless charset.empty? 
+        return str
+      end      
+      
+      return NKF.nkf(opt, str)
     end
 
   end
