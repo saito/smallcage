db9be75df53aa147b634cf14c8b45e2c400370d4
koseki <koseki@gmail.com>
2009-11-04 14:28:48 +0900

    add spec for update_list.

--- spec/update_list_spec.rb
+++ spec/update_list_spec.rb
@@ -5,7 +5,22 @@ describe SmallCage::UpdateList do
   root = Pathname.new(File.dirname(__FILE__) + "/data/updatelists")
 
   it "should create empty data" do
-    data = SmallCage::UpdateList.new(root + "dummy.yml", "/")
+    list = SmallCage::UpdateList.new(root + "dummy.yml", "/")
+    result = YAML.load(list.to_yaml)
+    result["version"].should match(/\d\.\d\.\d/)
+    result["list"].should be_empty
+
+    list = SmallCage::UpdateList.new(root + "dummy.yml", "/")
+    list.expire
+    result = YAML.load(list.to_yaml)
+    result["version"].should match(/\d\.\d\.\d/)
+    result["list"].should be_empty
+  end
+
+  it "should be update list items" do
+    list = SmallCage::UpdateList.new(root + "dummy.yml", "/")
+    result = YAML.load(list.to_yaml)
+    list.update("/abc/index.html.smc", 100, "/abc/index.html")
   end
 
 end
